456a213c7328eb625e6102fb58e7a9bc9c94485c
WilsonLiu <wilsonyat@gmail.com>
2026-06-20 18:36:05 +0800

    Delete template directory

--- template/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  DATA_URL: "data/daily.json",
-  LOG_URL: "https://script.google.com/macros/s/AKfycbwuRFFONEWIZ6yptI3H8_rPxMRIz4ysgjTGAjul_aK82HxbcZS4NPte1Gj0Iojv2XhX/exec",
-  DEFAULT_SYMBOL: "TWSE:2330",
-  TV_CHART_BASE: "https://tw.tradingview.com/chart/FKVV6Ftr/?symbol=",
-  SIGNAL_PREVIEW_LEN: 80
-};
